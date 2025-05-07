fe484cf08ebb093380f5df6f17c97ee922e6c861
Serge <sergembela@SergeMbela>
2025-05-07 20:22:55 +0200

    Add Xls services and dummy files for pdf and xls

---
+++ services/mocks_pdf.py
@@ -0,0 +1,12 @@
+# Exemple de statistiques à inclure dans le PDF
+stats = {
+    "Nombre total de patients": 1200,
+    "Nombre total de tests": 15000,
+    "Nombre total de cas positifs": 1200,
+    "Taux global de positivité": "8%",
+    "Taux de positivité vaccinés": "5%",
+    "Taux de positivité non vaccinés": "10%"
+}
+
+# Générer le PDF
+#generer_pdf_rapport(stats, "rapport_grippe_2024.pdf", titre="📊 Rapport Statistique - Grippe 2024", sous_titre="Résumé des résultats de l'année 2024")
---
+++ services/mocks_xls.py
@@ -0,0 +1,12 @@
+# Exemple de statistiques à inclure dans le fichier Excel
+stats = {
+    "Nombre total de patients": 1200,
+    "Nombre total de tests": 15000,
+    "Nombre total de cas positifs": 1200,
+    "Taux global de positivité": "8%",
+    "Taux de positivité vaccinés": "5%",
+    "Taux de positivité non vaccinés": "10%"
+}
+
+# Générer le fichier Excel
+#generer_excel_rapport(stats, "rapport_grippe_2024.xlsx", titre="📊 Rapport Statistique - Grippe 2024", sous_titre="Résumé des résultats de l'année 2024")
